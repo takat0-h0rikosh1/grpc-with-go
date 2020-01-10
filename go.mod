c16851ab399138c1ed7326f3faedbb24ec352681
堀越貴斗 <t_horikoshi@septeni-original.co.jp>
2020-01-10 20:07:36 +0900

    init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module grpc-with-go
+
+go 1.13
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/golang/protobuf v1.3.2
+	github.com/google/gops v0.3.6 // indirect
+	github.com/keybase/go-ps v0.0.0-20190827175125-91aafc93ba19 // indirect
+	github.com/shirou/gopsutil v2.19.12+incompatible // indirect
+	github.com/thoas/go-funk v0.5.0 // indirect
+	github.com/xlab/treeprint v0.0.0-20181112141820-a009c3971eca // indirect
+	golang.org/x/sys v0.0.0-20200107162124-548cf772de50 // indirect
+	google.golang.org/grpc v1.26.0
+	rsc.io/goversion v1.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/thoas/go-funk"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	pb "grpc-with-go/cafe"
+	"log"
+	"net"
+)
+
+const (
+	port = ":50052"
+)
+
+type server struct {
+	pb.UnimplementedCafeServer
+}
+
+type Product struct {
+	Name  string
+	Price int32
+}
+
+type Products struct {
+	Values []*Product
+}
+
+var products = Products{Values: []*Product{
+	&Product{Name: "coffee", Price: 100},
+	&Product{Name: "late", Price: 110},
+	&Product{Name: "mocha", Price: 120},
+}}
+
+func (pds *Products) getPrice(n string) (*int32, error) {
+	var r = funk.Find(pds.Values, func(r *Product) bool {
+		return r.Name == n
+	})
+	pd, ok := r.(*Product)
+	if ok {
+		return &pd.Price, nil
+	} else {
+		return nil, fmt.Errorf("There isn't %v in menus.", n)
+	}
+}
+
+func (s *server) Order(ctx context.Context, in *pb.OrderRequest) (*pb.OrderReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	p, err := (&products).getPrice(in.Name)
+	if err != nil {
+		log.Print(err)
+		return &pb.OrderReply{}, nil
+	} else {
+		return &pb.OrderReply{Price: *p}, nil
+	}
+
+}
+
+func (s *server) GetMenus(ctx context.Context, in *empty.Empty) (*pb.GetMenusReply, error) {
+	log.Print("Which drink do you want?")
+	var menus []*pb.Menu
+	for _, value := range products.Values {
+		menus = append(menus, &pb.Menu{Name: value.Name, Price: value.Price})
+	}
+	return &pb.GetMenusReply{Menus: menus}, nil
+}
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterCafeServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
